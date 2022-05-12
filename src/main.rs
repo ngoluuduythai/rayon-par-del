a8806efa1d27ccd41288c9959d8b8202ab46767f
ngoluuduythai <ngoluuduythai37@gmail.com>
2022-05-12 10:56:00 +0700

    delete content and parent directories

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use std::{fs::File, path};
+use std::{fs::{File, remove_dir}, path};
 use rayon::prelude::*;
 
 
 fn main() {
 
 
-     let directories = ["data", "data_2", "data_3", "data_4"];
+     let directories = ["data", "data_1","data_2", "data_3", "data-4", "data_14", "data_15", "data_16", "data_17", "data_18", "data_12", "data_13"];
 
      let _ = directories.par_iter()
         .map(|path| {
             println!("delete path {:?}", path);
-            std::fs::remove_dir_all(path)
+            std::fs::remove_dir_all(path);
+            std::fs::remove_dir(path)
         }).count();
         
 }
